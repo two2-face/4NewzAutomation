19c76d12085a386165af64202ecff8fa2bb02b8d
two2-face <t_r_musa@hotmail.com>
2025-04-29 18:29:35 +0200

    config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Zugangsdaten für WordPress REST API
+WORDPRESS_URL = "https://4newz.com"
+WP_USERNAME = "musadogan"
+WP_APP_PASSWORD = "apiforapp66"
+
+# Pixabay API-Key
+PIXABAY_API_KEY = "50006869-755b0edb7d48b739f674a758a"
+
+# RSS-Quellen für Tech-News
+RSS_FEEDS = [
+    "https://www.theverge.com/rss/index.xml",
+    "https://www.techradar.com/rss"
+]
