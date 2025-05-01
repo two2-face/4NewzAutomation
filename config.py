ed7f1092fe4a526ae5a5f225a620f7ae894454c9
two2-face <t_r_musa@hotmail.com>
2025-05-01 02:59:40 +0200

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Zugangsdaten für WordPress REST API
 WORDPRESS_URL = "https://4newz.com"
 WP_USERNAME = "musadogan"
-WP_APP_PASSWORD = "apiforapp66"
+WP_APP_PASSWORD = "vvJy 8lLy Wokr vk8x iyKS SbC3"
 
 # Pixabay API-Key
 PIXABAY_API_KEY = "50006869-755b0edb7d48b739f674a758a"
