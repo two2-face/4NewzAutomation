2f51b08e89ea91526412fbee24d8f408b056eba3
two2-face <t_r_musa@hotmail.com>
2025-05-03 17:20:29 +0200

    rss_parser.py

--- rss_parser.py
+++ rss_parser.py
@@ -1,13 +1,17 @@
 import feedparser
+from utils import generate_article_id, is_article_posted
 
-def get_latest_articles(feed_urls, limit_per_feed=2):
+def get_latest_articles(rss_urls, max_count=5):
     articles = []
-    for url in feed_urls:
+    for url in rss_urls:
         feed = feedparser.parse(url)
-        for entry in feed.entries[:limit_per_feed]:
-            articles.append({
-                "title": entry.title,
-                "link": entry.link,
-                "summary": entry.summary if 'summary' in entry else entry.description
-            })
+        for entry in feed.entries[:max_count]:
+            article_id = generate_article_id(entry.title, entry.link)
+            if not is_article_posted(article_id):
+                articles.append({
+                    "title": entry.title,
+                    "link": entry.link,
+                    "summary": entry.summary,
+                    "id": article_id
+                })
     return articles
