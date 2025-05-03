043a05834219bde8e7f2db52f4469683f0b0d385
two2-face <t_r_musa@hotmail.com>
2025-05-03 17:24:08 +0200

    onfig.py

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
 # Zugangsdaten für WordPress REST API
-WORDPRESS_URL = "https://4newz.com"
-WP_USERNAME = "musadogan"
-WP_APP_PASSWORD = "vvJy 8lLy Wokr vk8x iyKS SbC3"
-
-# Pixabay API-Key
-PIXABAY_API_KEY = "50006869-755b0edb7d48b739f674a758a"
-
-# RSS-Quellen für Tech-News
+OPENROUTER_API_KEY = "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"  # <- DEIN KEY
+PIXABAY_API_KEY = "50006869-755b0edb7d48b739f674a758a"               # <- Kostenlos auf pixabay.com/api
+WP_URL = "https://4newz.com"
+WP_USER = "musadogan"
+WP_PASSWORD = "vvJy 8lLy Wokr vk8x iyKS SbC3"
 RSS_FEEDS = [
-    "https://www.theverge.com/rss/index.xml",
+    ""https://www.theverge.com/rss/index.xml",
     "https://www.techradar.com/rss"
 ]
